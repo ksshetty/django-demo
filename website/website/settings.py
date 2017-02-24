2fa5da3d11a7627f73999d935bc5bb55fb7fc513
Karthik Shetty <karthikshetty88@gmail.com>
2017-02-24 14:00:52 -0800

    Add S3 configuration

--- website/website/settings.py
+++ website/website/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'g^ic@=ii08pq-g1ouh#*r=ic&ztb#m%60!4zdk9pyk2dvql_q%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['54.213.234.245']
 
 
 # Application definition
@@ -116,6 +116,73 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.10/howto/static-files/
+# https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_URL = '/static/'
+if 'S3_BUCKET_NAME' in os.environ:
+    AWS_HEADERS = {  # see http://developer.yahoo.com/performance/rules.html#expires
+        'Expires': 'Thu, 31 Dec 2034 20:00:00 GMT',
+        'Cache-Control': 'max-age=94608000',
+    }
+
+    AWS_STORAGE_BUCKET_NAME = os.environ['S3_BUCKET_NAME']
+    AWS_CLOUDFRONT_DOMAIN = os.environ['CLOUDFRONT_DOMAIN']
+    AWS_ACCESS_KEY_ID = os.environ['S3_ACCESS_KEY']
+    AWS_SECRET_ACCESS_KEY = os.environ['S3_SECRET_KEY']
+
+    # Tell django-storages that when coming up with the URL for an item in S3 storage, keep
+    # it simple - just use this domain plus the path. (If this isn't set, things get complicated).
+    # This controls how the `static` template tag from `staticfiles` gets expanded, if you're using it.
+    # We also use it in the next setting.
+    AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+    AWS_S3_HOST = 's3-us-west-2.amazonaws.com'
+
+    # This is used by the `static` template tag from `static`, if you're using that. Or if anything else
+    # refers directly to STATIC_URL. So it's safest to always set it.
+
+    # Tell the staticfiles app to use S3Boto storage when writing the collected static files (when
+    # you run `collectstatic`).
+
+    STATICFILES_LOCATION = 'static'
+    STATICFILES_STORAGE = 'website.custom_storages.StaticStorage'
+    # STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, STATICFILES_LOCATION)
+    STATIC_URL = '//%s/%s/' % (AWS_CLOUDFRONT_DOMAIN, STATICFILES_LOCATION)
+    # STATIC_URL = '/static/'
+    STATIC_PATH = os.path.join(BASE_DIR, 'static')
+    STATICFILES_DIRS = (
+        STATIC_PATH,
+    )
+
+    MEDIAFILES_LOCATION = 'media'
+    # MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
+    MEDIA_URL = '//%s/%s/' % (AWS_CLOUDFRONT_DOMAIN, MEDIAFILES_LOCATION)
+    DEFAULT_FILE_STORAGE = 'website.custom_storages.MediaStorage'
+    print "Using Aws  S3 config"
+else:
+    STATIC_URL = '/static/'
+
+    STATIC_PATH = os.path.join(BASE_DIR, 'static')
+    STATICFILES_DIRS = (
+        STATIC_PATH,
+    )
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static_static/')
+
+    MEDIA_URL = '/media/'
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+LOGIN_URL = '/login/'
+
+REST_FRAMEWORK = {
+    # Filters
+    'DEFAULT_FILTER_BACKENDS': ('rest_framework.filters.DjangoFilterBackend',),
+
+    # Test framework
+    'TEST_REQUEST_DEFAULT_FORMAT': 'json',
+
+    # Auth
+    'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework.authentication.SessionAuthentication',
+                                       'rest_framework.authentication.TokenAuthentication',),
+
+    # Pagination
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 25
+}
